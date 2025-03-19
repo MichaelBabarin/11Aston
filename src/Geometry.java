84f282f013223309fce9502a6a8c24f402d48fac
MichaelBabarin <108865819+MichaelBabarin@users.noreply.github.com>
2025-03-20 00:41:44 +0300

    Update Geometry.java

--- src/Geometry.java
+++ src/Geometry.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+public class GeometricShapes {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5, "Красный", "Синий");
+        Shape rectangle = new Rectangle(4, 6, "Зеленый", "Желтый");
+        Shape triangle = new Triangle(3, 4, 5, "Фиолетовый", "Розовый");
 
-public class Geometry {
+        System.out.println("Круг:");
+        circle.printShapeInfo();
+        System.out.println();
+
+        System.out.println("Прямоугольник:");
+        rectangle.printShapeInfo();
+        System.out.println();
+
+        System.out.println("Треугольник:");
+        triangle.printShapeInfo();
+    }
 }
